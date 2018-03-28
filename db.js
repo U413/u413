e26797201b8c39a9835de14cd50e2e9428da2592
ConsciousCode <conscious.code@gmail.com>
2018-03-28 18:25:48 -0400

    Generalized db.js

--- db.js
+++ db.js
@@ -4,44 +4,35 @@ const
 const client = new Client();
 client.connect();
 
-function run_sql(q, ok, no) {
-	client.query(run_file.cache[fn], (err, rows) => {
+function query_promise(q, args, ok, no) {
+	client.query(q, args, (err, rows) => {
 		if(err) no(err)
 		else ok(rows);
 	});
 }
 
-function run_file(fn) {
+function query(fn, ...args) {
 	return new Promise((ok, no) => {
 		if(fn in run_file.cache) {
-			run_sql(run_file.cache[fn])
-			client.query(run_file.cache[fn], (err, rows) => {
-				if(err) no(err)
-				else ok(rows);
-			});
+			query_promise(run_file.cache[fn], args, ok, no);
 		}
 		else {
-			fs.read(fn, (err, data) => {
+			fs.read(fn + ".sql", (err, data) => {
 				if(err) {
 					no(err);
 				}
 				else {
-					
-					client.query(data.toString(), (err, rows) => {
-						if(err) no(err)
-						else ok(data);
-					});
+					query_promise(
+						run_file.cache[fn] = data.toString(),
+						args, ok, no
+					);
 				}
 			});
 		}
 	});
 }
-run_file.cache = {};
-
-function init() {
-	return run_file("init.sql");
-}
+query.cache = {};
 
 module.exports = {
-	init
+	query
 };
