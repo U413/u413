28b92a34b6823695474ec74c685caf3aee3e45ac
ConsciousCode <conscious.code@gmail.com>
2018-04-18 17:24:01 -0400

    Remove file if it exists

--- ipc.js
+++ ipc.js
@@ -7,13 +7,28 @@
 const
 	fs = require("fs"),
 	net = require("net");
+	
+const ipcname = 'private/ipc.sock';
 
-net.createServer(conn => {
-	conn.on('data', data => {
-		data = data + "";
-		switch(data) {
-			case 'redeploy': return app.redeploy();
-			
-		}
-	});
-}).listen("private/ipc.sock");
+function mkserver() {
+	net.createServer(conn => {
+		conn.on('data', data => {
+			data = data + "";
+			switch(data) {
+				case 'redeploy': return app.redeploy();
+				
+			}
+		});
+	}).listen(ipcname);
+}
+
+fs.stat(ipcname, (err, stats) => {
+	if(stats) {
+		fs.unlink(ipcname, () => {
+			mkserver();
+		})
+	}
+	else {
+		mkserver();
+	}
+})
