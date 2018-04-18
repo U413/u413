a13f6afdeb1612ade79d1b4e8f296a2c4cb2fddc
ConsciousCode <conscious.code@gmail.com>
2018-04-18 16:21:42 -0400

    Redeploy fix?????????

--- tools/redeploy.sh
+++ tools/redeploy.sh
@@ -7,23 +7,16 @@ disown
 GIT=/usr/bin/git
 NPM=npm
 
-echo "KILLING"
 # Kill the host node process
 kill $1
-
-echo "GIT"
 
 # Fetch the new git data
 $GIT fetch origin master
 $GIT reset --hard FETCH_HEAD
 $GIT clean -df
 
-echo "NPM"
-
 # Install any new npm packages
 $NPM install
 
-echo "REDEPLOY"
-
 # Run the server (defer to run.sh)
-/bin/bash ./run-disown.sh
+/bin/bash ./tools/run-disown.sh
