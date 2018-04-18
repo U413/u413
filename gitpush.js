f43a220582f23608aceafb47d270c7f35fae8a66
ConsciousCode <conscious.code@gmail.com>
2018-04-18 16:48:56 -0400

    Just parse the body directly to perform HMAC

--- gitpush.js
+++ gitpush.js
@@ -14,31 +14,41 @@ const
 
 const router = module.exports = new express.Router();
 
+log.info("init git webhook redeploy");
+
 // Catch an IO error if private/gitpush.secret doesn't exist
 try {
-	log.info("init git webhook redeploy");
-	
-	const auth = crypto.createHMAC('sha1',
-		fs.readFileSync("private/gitpush.secret") + ""
-	);
-
-	router.use("/!!!gitpush!!!", (req, res, next) => {
-		log.info("Request for !!!gitpush!!!");
-		res.end("Success");
-		console.log(req.headers, req.body);
-		//if(req.get("X-Hub-Signature") === secret) {
-			// TODO: save session data
-			log.info("Redeploying server...");
-			spawn("/bin/bash", ["tools/redeploy.sh", process.pid], {
-				detached: true,
-				stdio: 'inherit'
-			}).unref();
-		//}
-		//else {
-			//next();
-		//}
-	});
-}
+	var secret = fs.readFileSync("private/gitpush.secret") + "";
 catch(e) {
 	log.info("Failed to init git webhook redeploy, missing file");
+	return;
 }
+
+const auth = crypto.createHMAC('sha1', secret);
+
+router.use("/!!!gitpush!!!", (req, res, next) => {
+	log.info("Request for !!!gitpush!!!");
+	res.end("Success");
+	
+	let body = "";
+	req.on('data', chunk => {
+		body += chunk;
+	});
+	req.on('end', () => {
+		auth.update(body);
+		console.log("Digest:", auth.digest('hex'));
+	})
+	
+	console.log(req.headers);
+	//if(req.get("X-Hub-Signature") === secret) {
+		// TODO: save session data
+		log.info("Redeploying server...");
+		spawn("/bin/bash", ["tools/redeploy.sh", process.pid], {
+			detached: true,
+			stdio: 'inherit'
+		}).unref();
+	//}
+	//else {
+		//next();
+	//}
+});
