46b1450f5e23359eee8404f8c5cf334c6e98a9e2
k <mwgamera@gmail.com>
2018-05-07 03:59:02 +0000

    Fix genpwd

--- docker/initdb/1-genpwd.sh
+++ docker/initdb/1-genpwd.sh
@@ -2,7 +2,6 @@
 
 echo Generating database password
 PRIV=/var/www/private
-mkdir -p $PRIV
-dd if=/dev/urandom of=$PRIV/u413.pwd bs=1 count=16
-PWD=$(cat $PRIV/u413.pwd)
-psql -c "alter user password '${PWD//'/\\}';"
+mkdir -p "$PRIV"
+dd if=/dev/urandom bs=18 count=1 | base64 > "$PRIV/u413.pwd"
+psql -c "alter user $USER password '$(<"$PRIV/u413.pwd")';"
